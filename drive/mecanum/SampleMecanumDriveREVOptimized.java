788ce9a6657c892939d4324b2f9069bdd47540c8
Aidan Ferry <aidanferry02@gmail.com>
2020-01-12 13:47:42 -0500

    Initial commit

---
+++ drive/mecanum/SampleMecanumDriveREVOptimized.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode.drive.mecanum;
+
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.getMotorVelocityF;
+
+import android.support.annotation.NonNull;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.teamcode.util.AxesSigns;
+import org.firstinspires.ftc.teamcode.util.BNO055IMUUtil;
+import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
+import org.openftc.revextensions2.ExpansionHubEx;
+import org.openftc.revextensions2.ExpansionHubMotor;
+import org.openftc.revextensions2.RevBulkData;
+
+/*
+ * Optimized mecanum drive implementation for REV ExHs. The time savings may significantly improve
+ * trajectory following performance with moderate additional complexity.
+ */
+public class SampleMecanumDriveREVOptimized extends SampleMecanumDriveBase {
+    private ExpansionHubEx conhub;
+    private ExpansionHubMotor leftFront, leftRear, rightRear, rightFront;
+    //private ExpansionHubEx exphub;
+    private List<ExpansionHubMotor> motors;
+    private BNO055IMU imu;
+
+    public SampleMecanumDriveREVOptimized(HardwareMap hardwareMap) {
+        super();
+
+        LynxModuleUtil.ensureMinimumFirmwareVersion(hardwareMap);
+        // for simplicity, we assume that the desired IMU and drive motors are on the same hub
+        // if your motors are split between hubs, **you will need to add another bulk read**
+        conhub = hardwareMap.get(ExpansionHubEx.class, "Control Hub");
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+
+        // upward (normal to the floor) using a command like the following:
+        BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.NPN);
+
+        leftFront = hardwareMap.get(ExpansionHubMotor.class, "front_left");
+        leftRear = hardwareMap.get(ExpansionHubMotor.class, "rear_left");
+        rightRear = hardwareMap.get(ExpansionHubMotor.class, "rear_right");
+        rightFront = hardwareMap.get(ExpansionHubMotor.class, "front_right");
+
+        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
+
+        for (ExpansionHubMotor motor : motors) {
+            if (RUN_USING_ENCODER) {
+                motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+
+        if (RUN_USING_ENCODER && MOTOR_VELO_PID != null) {
+            setPIDCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, MOTOR_VELO_PID);
+        }
+
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // TODO: if desired, use setLocalizer() to change the localization method
+        // for instance, setLocalizer(new ThreeTrackingWheelLocalizer(...));
+    }
+
+    @Override
+    public PIDCoefficients getPIDCoefficients(DcMotor.RunMode runMode) {
+        PIDFCoefficients coefficients = leftFront.getPIDFCoefficients(runMode);
+        return new PIDCoefficients(coefficients.p, coefficients.i, coefficients.d);
+    }
+
+    @Override
+    public void setPIDCoefficients(DcMotor.RunMode runMode, PIDCoefficients coefficients) {
+        for (ExpansionHubMotor motor : motors) {
+            motor.setPIDFCoefficients(runMode, new PIDFCoefficients(
+                    coefficients.kP, coefficients.kI, coefficients.kD, getMotorVelocityF()
+            ));
+        }
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelPositions() {
+        RevBulkData bulkData = conhub.getBulkInputData();
+
+        if (bulkData == null) {
+            return Arrays.asList(0.0, 0.0, 0.0, 0.0);
+        }
+
+        List<Double> wheelPositions = new ArrayList<>();
+        for (ExpansionHubMotor motor : motors) {
+            wheelPositions.add(encoderTicksToInches(bulkData.getMotorCurrentPosition(motor)));
+        }
+        return wheelPositions;
+    }
+
+    @Override
+    public List<Double> getWheelVelocities() {
+        RevBulkData bulkData = conhub.getBulkInputData();
+
+        if (bulkData == null) {
+            return Arrays.asList(0.0, 0.0, 0.0, 0.0);
+        }
+
+        List<Double> wheelVelocities = new ArrayList<>();
+        for (ExpansionHubMotor motor : motors) {
+            wheelVelocities.add(encoderTicksToInches(bulkData.getMotorVelocity(motor)));
+        }
+        return wheelVelocities;
+    }
+
+    @Override
+    public void setMotorPowers(double v, double v1, double v2, double v3) {
+        leftFront.setPower(v);
+        leftRear.setPower(v1);
+        rightRear.setPower(v2);
+        rightFront.setPower(v3);
+    }
+
+    @Override
+    public double getRawExternalHeading() {
+        return imu.getAngularOrientation().firstAngle;
+    }
+}
